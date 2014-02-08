c1cbb6bb603b8bcc94665e29f5a73411dbb4d641
rachelkelly <rachelkelly@users.noreply.github.com>
2014-02-08 12:10:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ print "how many friends "
 number = raw_input("> ")
 
 while i < number:
-  print "who is the friend "
+	print "who is the friend "
 	friend = raw_input("> ")
 	friends.append(friend)
 	print friends
-	print "%s left to input" % number
-	print i
-	i += 1
-	print i
+	i = i + 1
 
 print "here's all the friends: "
 print friends
