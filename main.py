6c94fdb491f6129f471283d0abb34317a4c4e642
rachelkelly <rachelkelly@users.noreply.github.com>
2014-02-15 13:54:25 -0800

    IT WORKS!!  now to clean up, optimize & write cmts

--- main.py
+++ main.py
@@ -36,13 +36,37 @@ while newIndex > 0:
 	print "adding guy above to brunch"
 	list.pop(friendForIndex)
 	newIndex = newIndex - 1
-	print brunch
+	print "brunch folxxx: %s" % brunch
+
+# now the question becomes, can I use the same variable if I just redefine it a bunch
+# actually I think the question is will it keep looping the if var below?
+# I don't think it will.
 
 	if newIndex > 0:
-		print friendForIndex[-1]
 		appender = friendForIndex[-1]
 		dinner.append(appender)
 		print "adding guy above to dinner"
 		list.pop(friendForIndex)
 		newIndex = newIndex - 1
-		print dinner
+		print "dinner folxxx: %s" % dinner
+
+	if newIndex > 0:
+		appender = friendForIndex[-1]
+		breakfast.append(appender)
+		print "adding guy above to breakfast"
+		list.pop(friendForIndex)
+		newIndex = newIndex - 1
+		print "breakfast folxxx: %s" % breakfast
+
+	if newIndex > 0:
+		appender = friendForIndex[-1]
+		snax.append(appender)
+		print "adding guy above to snack duty"
+		list.pop(friendForIndex)
+		newIndex = newIndex - 1
+		print "snaxfolxxx: %s" % snax
+
+print "people on brunch: %s" % brunch
+print "people on snax: %s" % snax
+print "people on dinner: %s" % dinner
+print "people on breakfast: %s" % breakfast
