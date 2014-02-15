1e6119574d6566ab70b44349132dc78fb0c3879e
rachelkelly <rachelkelly@users.noreply.github.com>
2014-02-15 11:58:55 -0800

    ha ha doesn't work yet.  see extended desc
    so I think a better way to do this would be to first randomize the list and the pop item after item off, rather than trying to make friendChoice find something random within the list every time.  georgia also suggests "find," but I still think that the issue there would be to.... oh I could have it tell me the position once I FIND it and then del at that position.
    
    still I think it would be easier to randomize the list and pop off the last item one by one.
    
    committed because it's still a bunch of lines, man

--- main.py
+++ main.py
@@ -15,9 +15,39 @@ while i < howmany:
 
 print "here's all the friends: "
 print friends
+friendListSize = len(friends)
+print "there are %s friends" % friendListSize
+#so now all the friends are in.  this can probably be shortened
+#NOW I want to lump them all into meal assignments
 
+newIndex = friendListSize
+friendForIndex = friends
 brunch = []
 snax = []
 dinner = []
 breakfast = []
 
+#so here I'll just make each list append a random friend
+#there is CERTAINLY a better way to do this, probably
+#involving a big list of lists, like, "pick a list & add
+#the friend to it,"
+#### but the issue there is that the pigeonhole will fail!
+#### and it won't be even!  god, I guess I could do some
+#### kind of gnarly conditional "if len(brunch) > len(snax)"
+#### and every possible combination for every random friend
+#### choice add, oh my god that is a terrible idea ok no.
+
+while newIndex > 0:
+	friendChoice = random.choice(friendForIndex)
+	print "adding %s to brunch which already contains %s" % (friendChoice, brunch)
+	brunch.append(friendChoice)
+	friendForIndex = friendForIndex - friendChoice
+	print "adding %s to snax which already contains %s" % (friendChoice, snax)
+	snax.append(friendChoice)
+	friendForIndex = friendForIndex - friendChoice
+	print "adding %s to dinner which already contains %s" % (friendChoice, dinner)
+	dinner.append(friendChoice)
+	friendForIndex = friendForIndex - friendChoice
+	print "adding %s to breakfast which already contains %s" % (friendChoice, breakfast)
+	breakfast.append(friendChoice)
+	friendForIndex = friendForIndex - friendChoice
