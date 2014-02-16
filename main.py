8ed8460897123a14c1efe5d713da976b4ace3e77
rachelkelly <rachelkelly@users.noreply.github.com>
2014-02-15 17:32:47 -0800

    trying to figure out nested iteration

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ while i < howmany:
 	print "who is the friend "
 	friend = raw_input("> ")
 	friends.append(friend)
-	print friends
 	i = i + 1
 
 print "here's all the friends: "
@@ -17,56 +16,22 @@ print friends
 friendListSize = len(friends)
 print "there are %s friends" % friendListSize
 
+shuffledFriends = friends
+random.shuffle(shuffledFriends)
+# had a print statement here that printed out the shuffledfriends
 
-friendForIndex = friends
-random.shuffle(friendForIndex)
-print friendForIndex
-friendForIndex[-1]
+# or should structure MEALS be a dict so that its keys can be called?
+# OMG PROBABLY
 
 brunch = []
 snax = []
 dinner = []
 breakfast = []
+meals = [brunch, snax, dinner, breakfast]
 
-newIndex = friendListSize
-while newIndex > 0:
+print meals
 
-	appender = friendForIndex[-1]
-	brunch.append(appender)
-	print "adding guy above to brunch"
-	list.pop(friendForIndex)
-	newIndex = newIndex - 1
-	print "brunch folxxx: %s" % brunch
-
-# now the question becomes, can I use the same variable if I just redefine it a bunch
-# actually I think the question is will it keep looping the if var below?
-# I don't think it will.
-
-	if newIndex > 0:
-		appender = friendForIndex[-1]
-		dinner.append(appender)
-		print "adding guy above to dinner"
-		list.pop(friendForIndex)
-		newIndex = newIndex - 1
-		print "dinner folxxx: %s" % dinner
-
-	if newIndex > 0:
-		appender = friendForIndex[-1]
-		breakfast.append(appender)
-		print "adding guy above to breakfast"
-		list.pop(friendForIndex)
-		newIndex = newIndex - 1
-		print "breakfast folxxx: %s" % breakfast
-
-	if newIndex > 0:
-		appender = friendForIndex[-1]
-		snax.append(appender)
-		print "adding guy above to snack duty"
-		list.pop(friendForIndex)
-		newIndex = newIndex - 1
-		print "snaxfolxxx: %s" % snax
-
-print "people on brunch: %s" % brunch
-print "people on snax: %s" % snax
-print "people on dinner: %s" % dinner
-print "people on breakfast: %s" % breakfast 
+k = friendListSize
+for j in meals and k:
+	print "should print %s times" % friendListSize
+	k = k - 1
